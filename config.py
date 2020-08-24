ed23ee24f8ecdc2c5190f913f4bc938bbbbb97ce
neelabh17 <neelabh.madan@outlook.com>
2020-08-24 16:00:33 +0530

    final changes

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ config = {
     'starting_epoch' : 0,
     'batch_size' : 1,
     'log_val' : 10,
-    'task' : 'abnormal',
+    'task' : 'abnormal', # "meniscus" and  "acl" are the other options
     'weight_decay' : 0.01,
     'patience' : 5,
     'save_model' : 1,
